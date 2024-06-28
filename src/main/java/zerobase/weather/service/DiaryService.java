241e0ac22c9b1efb49fff25f246b7524cf7fec7f
Jimin Lee <rjsb0648@gmail.com>
2024-06-28 15:34:07 +0900

    transaction

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -53,6 +53,7 @@ public class DiaryService {
         return diaryRepository.findAllByDate(date);
     }
 
+    @Transactional(readOnly = true)
     public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate) {
         return diaryRepository.findAllByDateBetween(startDate, endDate);
     }
